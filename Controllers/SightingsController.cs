95aa192d33c22f36e0facf3fe807989221b43820
space-commits <57108281+space-commits@users.noreply.github.com>
2024-04-25 15:00:04 +0100

    details and delate

--- Controllers/SightingsController.cs
+++ Controllers/SightingsController.cs
@@ -16,26 +16,85 @@ namespace WildlifeMVC.Controllers
 {
     public class SightingsController : Controller
     {
+
+        private async Task<HttpResponseMessage> deleteHttpResponse(string target)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri("http://localhost:64341/api/");
+                return await httpClient.DeleteAsync($"WildlifeSightings/{target}");
+            }
+        }
+
+        private async Task<HttpResponseMessage> postHttpResponse(string target, StringContent data)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri("http://localhost:64341/api/");
+                return await httpClient.PostAsync($"WildlifeSightings/{target}", data);
+            }
+        }
+
+        private async Task<HttpResponseMessage> getHttpResponse(string target)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri("http://localhost:64341/api/");
+                return await httpClient.GetAsync($"WildlifeSightings/{target}");
+            }
+        }
+
+        [HttpGet]
+        public async Task<SightingAPIModel> GetSightingByID(int id)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpResponseMessage response = await getHttpResponse($"BySighting/{id}");
+                SightingAPIModel apiData = await response.Content.ReadAsAsync<SightingAPIModel>();
+                if (response.IsSuccessStatusCode)
+                {
+                    return apiData;
+                }
+                else
+                {
+                    throw new HttpException(404, "No sighting found with that ID");
+                }
+            }
+        }
+
+
         [HttpGet]
         public async Task<ActionResult> SightingsIndex()
         {
-            IEnumerable <SightingAPIModel> apiData = new List<SightingAPIModel>();
+        
             using (HttpClient httpClient = new HttpClient())
             {
-                try
+                IEnumerable<SightingAPIModel> apiData = new List<SightingAPIModel>();
+                HttpResponseMessage response = await getHttpResponse("All");
+                apiData = await response.Content.ReadAsAsync<IEnumerable<SightingAPIModel>>();
+                if (response.IsSuccessStatusCode)
                 {
-                    httpClient.BaseAddress = new Uri("http://localhost:64341/api/");
-                    HttpResponseMessage response = await httpClient.GetAsync("WildlifeSightings/All");
-                    response.EnsureSuccessStatusCode(); //only proceeds if response is successful 
-                    apiData = await response.Content.ReadAsAsync<IEnumerable<SightingAPIModel>>();
+                    return View(apiData);
                 }
-                catch (Exception ex)
+                else
                 {
-                    return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.Message);
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
                 }
             }
+        }
 
-            return View(apiData);
+        public async Task<ActionResult> SightingDetails(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SightingAPIModel sighting = await GetSightingByID((int)id);
+            if (sighting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sighting);
         }
 
         public ActionResult CreateSighting()
@@ -47,49 +106,25 @@ namespace WildlifeMVC.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> CreateSighting(SightingAPIModel sighting)
         {
-            try
+            //should instead return create view, and have the form display warnrings about invalid inputs
+            if (!ModelState.IsValid)
             {
-                //should instead return create view, and have the form display warnrings about invalid inputs
-                if (!ModelState.IsValid)
-                {
-                    throw new HttpException(400, "Form data was invalid");
-                }
-
-                using (HttpClient httpClient = new HttpClient())
-                {
-                    httpClient.BaseAddress = new Uri("http://localhost:64341/api/");
-                    StringContent data = new StringContent(JsonConvert.SerializeObject(sighting), Encoding.UTF8, "application/json");
-                    HttpResponseMessage response = await httpClient.PostAsync("WildlifeSightings/CreateSighting", data);
-                    response.EnsureSuccessStatusCode();
-                    return RedirectToAction("SightingsIndex");
-                }
+                throw new HttpException(400, "Form data was invalid");
             }
-            catch (Exception ex) 
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.Message);
-            }
-        }
 
-        [HttpGet]
-        public async Task<SightingAPIModel> GetSightingByID(int id)
-        {
-            SightingAPIModel apiData;
             using (HttpClient httpClient = new HttpClient())
             {
-                try
+                StringContent data = new StringContent(JsonConvert.SerializeObject(sighting), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await postHttpResponse("CreateSighting", data);
+                if (response.IsSuccessStatusCode)
                 {
-                    httpClient.BaseAddress = new Uri("http://localhost:64341/api/");
-                    HttpResponseMessage response = await httpClient.GetAsync($"WildlifeSightings/BySighting/{id}");
-                    response.EnsureSuccessStatusCode(); //only proceeds if response is successful 
-                    apiData = await response.Content.ReadAsAsync<SightingAPIModel>();
+                    return RedirectToAction("SightingsIndex");
                 }
-                catch
+                else
                 {
-                    throw new HttpException(404, "No sighting found with that ID");
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
                 }
             }
-
-            return apiData;
         }
 
         public async Task<ActionResult> UpdateSighting(int? id)
@@ -110,26 +145,56 @@ namespace WildlifeMVC.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> UpdateSighting([Bind(Include = "ID,SpeciesID,XCoordinate,YCoordinate,TimeStamp,Description,Locaton,County")] SightingAPIModel sighting)
         {
-            try
+            //should instead return create view, and have the form display warnrings about invalid inputs
+            if (!ModelState.IsValid)
             {
-                //should instead return create view, and have the form display warnrings about invalid inputs
-                if (!ModelState.IsValid)
-                {
-                    throw new HttpException(400, "Update data was invalid");
-                }
+                throw new HttpException(400, "Update data was invalid");
+            }
 
-                using (HttpClient httpClient = new HttpClient())
+            using (HttpClient httpClient = new HttpClient())
+            {
+                StringContent data = new StringContent(JsonConvert.SerializeObject(sighting), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await postHttpResponse("UpdateSighting", data);
+                if (response.IsSuccessStatusCode)
                 {
-                    httpClient.BaseAddress = new Uri("http://localhost:64341/api/");
-                    StringContent data = new StringContent(JsonConvert.SerializeObject(sighting), Encoding.UTF8, "application/json");
-                    HttpResponseMessage response = await httpClient.PostAsync("WildlifeSightings/UpdateSighting", data);
-                    response.EnsureSuccessStatusCode();
                     return RedirectToAction("SightingsIndex");
                 }
+                else
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
             }
-            catch (Exception ex)
+        }
+
+        public async Task<ActionResult> DeleteSighting(int? id)
+        {
+            if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.Message);
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SightingAPIModel sighting = await GetSightingByID((int)id);
+            if (sighting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sighting);
+        }
+
+        [HttpPost, ActionName("DeleteSighting")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteSighting(int id) 
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpResponseMessage response = await deleteHttpResponse($"DeleteSighting/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("SightingsIndex");
+                }
+                else
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
             }
         }
     }
